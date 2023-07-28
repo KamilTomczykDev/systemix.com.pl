d8f7b826c7f8f4679b3c505e5730ca6d339238d8
Kamil Tomczyk <kamiltomczyk.dev@gmail.com>
2023-07-28 12:30:13 +0200

    products tab component added

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ const nav = document.querySelector("nav");
 const allSections = document.querySelectorAll(".reveal__section");
 const header = document.querySelector(".header");
 const navHeight = nav.getBoundingClientRect().height;
+const tabsContainer = document.querySelector(".operations__tab-container");
+const tabs = document.querySelectorAll(".operations__tab");
+const tabsContent = document.querySelectorAll(".operations__content");
 
 //Smooth Loading left side of nav
 
@@ -64,4 +67,25 @@ const sectionObserver = new IntersectionObserver(reveal, {
 
 allSections.forEach(function (section) {
   sectionObserver.observe(section);
+});
+
+//TABS
+
+tabsContainer.addEventListener("click", function (e) {
+  const clicked = e.target.closest(".operations__tab");
+
+  // Guard clause
+  if (!clicked) return;
+
+  // Remove active classes
+  tabs.forEach((t) => t.classList.remove("operations__tab--active"));
+  tabsContent.forEach((c) => c.classList.remove("operations__content--active"));
+
+  // Activate tab
+  clicked.classList.add("operations__tab--active");
+
+  // Activate content area
+  document
+    .querySelector(`.operations__content--${clicked.dataset.tab}`)
+    .classList.add("operations__content--active");
 });
